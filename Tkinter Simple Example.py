ab34eaffb21379a4ce55c80336bcffb33b545596
TOOMUCHBREW <toomuchbrew@gmail.com>
2015-05-19 16:32:43 -0400

    Tkinter Example v2
    From Zetcode.com

--- Tkinter Simple Example.py
+++ Tkinter Simple Example.py
@@ -16,19 +16,26 @@ website: www.zetcode.com
 '''
 
 from Tkinter import Tk, Frame, BOTH
+from ttk import Frame, Style, Button
 
 class Example(Frame):
 
     def __init__(self, parent):
-        Frame.__init__(self, parent, background = "white")
+        Frame.__init__(self, parent) ##, background = "white")
 
         self.parent = parent
 
         self.initUI()
 
     def initUI(self):
-        self.parent.title("Simple")
+        self.parent.title("Quit Button")
+        self.style = Style()
+        self.style.theme_use("default")
+
         self.pack(fill=BOTH, expand = 1)
+
+        quitButton = Button(self, text = "Quit", command = self.quit)
+        quitButton.place(x =50, y = 50)
 
 
 def main():
